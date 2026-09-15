db3588ecfb03bd59953cefb9646c3e7d31438bbc
SaltyMold <168920293+SaltyMold@users.noreply.github.com>
2026-09-15 23:03:49 +0200

    wtf

--- tmp_compile_test.cpp
+++
@@ -1,7 +0,0 @@
-#include "shared/kandinsky/include/kandinsky/color.h"
-#include "epsilon/escher/include/escher/palette.h"
-
-int main() {
-  (void)Escher::Palette::numberOfDataColors();
-  return 0;
-}
